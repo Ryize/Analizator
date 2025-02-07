213bb00c372fa4e3a30c8d8aee2b6f1df2caeb79
PetrTimokhin <ptimohin01@gmail.com>
2025-02-07 14:51:49 +0300

    feat: добавление navbar_menu, главного меню сайта;

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask import Flask, render_template
 
-from config import DATABASE_URL, SECRET_KEY
 
-# Создание экземпляра приложения Flask и настройка его основных параметров
-# и подключения к базе данных
 app = Flask(__name__)
 
-# Настройка базы данных SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
-# Задание секретного ключа для защиты сессий
-app.config['SECRET_KEY'] = SECRET_KEY
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('navbar_menu.html')
 
-# Инициализация базы данных
-db = SQLAlchemy(app)
 
-# Инициализация менеджера логина
-manager = LoginManager(app)
+if __name__ == '__main__':
+    app.run()
+
